f788fefcfbcf4c53d50c202b6f188b938a11539a
Paulo Carneiro <paulocarneirosegundo@gmail.com>
2024-09-26 00:41:36 -0300

    Projeto Iniciado

---
+++ src/main/java/com/instantes/instantes/InstantesApplication.java
@@ -0,0 +1,13 @@
+package com.instantes.instantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstantesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/instantes/instantes/InstantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instantes.instantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
